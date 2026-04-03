c0e95a13ec3c3625338ad025ab41c38f6ace53f5
Max Parisi <makermax101@gmail.com>
2026-04-02 20:07:17 -0700

    add splash screen after 1hr of work :)

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 const { app, BrowserWindow, dialog } = require("electron");
 const path = require("path");
 const { startServer } = require("./server");
-const Config = require("./config.json");
 const Type = require("./type");
 
 let win;
 
-function createWindow(port) {
+function createWindow() {
     win = new BrowserWindow({
         width: 1200,
         height: 800,
+        backgroundColor: "#171717", // middle of gradient applied by CSS
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
@@ -17,7 +17,7 @@ function createWindow(port) {
     });
 
     // Load the admin page from localhost
-    win.loadURL(`http://localhost:${port}/admin`);
+    win.loadFile(path.join(__dirname, "public", "splash", "index.html"));
 
     Type.blankKeypress(); // to bring up permissions dialogue at start
 
@@ -27,33 +27,17 @@ function createWindow(port) {
 }
 
 app.whenReady().then(async () => {
-    startServer()
-        .then((port) => {
-            createWindow(port);
-        })
-        .catch((err) => {
-            console.error(err);
+    createWindow();
 
-            dialog.showErrorBox(
-                'CollaboKeys Error',
-                err?.message || "Unknown Error"
-            );
-            process.exit(1);
-            return;
-        });
-    /*try {
+    try {
         const port = await startServer();
-        createWindow(port);
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // flash splash screen
+        win.loadURL(`http://localhost:${port}/admin`);
     } catch (err) {
         console.error(err);
-
-        dialog.showErrorBox(
-            'CollaboKeys Error',
-            err?.message || "Unknown Error"
-        );
-
-        return;
-    }*/
+        dialog.showErrorBox( "CollaboKeys Error", err?.message || "Unknown Error" );
+        process.exit(1);
+    }
 });
 
 app.on('window-all-closed', () => {
